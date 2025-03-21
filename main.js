90f6bd527303b808472e6a24cda7698c2cf23a7c
Jennifer O'Leary <165350064+jennyoleary87@users.noreply.github.com>
2025-03-21 11:00:26 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+/*
+03/21/2025
+
+Main Points:
+- Be able to filter (filter() from Using Arrays) types you want to search for: entertainment, food, water activities, etc
+- Enter where you are staying and for how long so the program can currate an ideal itinerary
+that goes with what you are interested in and takes into account how far you are willing do drive;
+knows the distance from where you are staying
+- Build an array (from Building Arrays) to hold locations such as resturaunts and arcades. Include phone number and map location for each place. 
+
+*/
+
